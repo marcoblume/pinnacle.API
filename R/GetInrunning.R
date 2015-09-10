9128338a4c17746d1195af9ad489b2eb065281ca
Nicholas Jhirad <Nicholas.Jhirad@pinnaclesports.com>
2015-09-10 13:54:19 -0700

    Added GetInrruning

---
+++ R/GetInrunning.R
@@ -0,0 +1,28 @@
+#' GetInrunning
+#'
+#' @return A dataframe containing the current State of live events
+#' @import httr
+#' @importFrom jsonlite fromJSON
+#' @export
+#'
+#' @examples
+#' \donttest{
+#' SetCredentials("TESTAPI","APITEST")
+#' AcceptTermsAndConditions(accepted=TRUE)
+#' GetInrunning()
+#' }
+GetInrunning <- function() {
+  r <- GET(paste0(.PinnacleAPI$url ,"/v1/inrunning"),
+           add_headers(Authorization= authorization(),
+                       "Content-Type" = "application/json"))
+  res <-  jsonlite::fromJSON(content(r,type="text"),simplifyVector=FALSE)
+  inrunningState <- fixPeriods(res,depth = 5)
+  inrunningState <- combineFactors(inrunningState,depth = 4)
+  inrunningState <- fixPeriods(inrunningState,depth = 3)
+  inrunningState <- combineFactors(inrunningState,depth=2)
+  inrunningState <- fixPeriods(inrunningState,depth=1)
+  inrunningState <- combineFactors(inrunningState,depth=0)
+  
+  names(inrunningState)[1:3] = c('SportID','LeagueID','EventID')
+  return(inrunningState)
+}
