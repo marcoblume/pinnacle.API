43a90f1e97d81aa9419ecb4af501b16f96346384
unknown <nicholasj@HQ-TRD-17.planetpinnacle.com>
2015-09-11 09:24:12 -0700

    missing from last commit

--- R/showOddsFunctions.R
+++ R/showOddsFunctions.R
@@ -2,50 +2,37 @@
 #'
 #' @param x a list
 #' @param depth numeric value of the number of sublist
-#'
+#' @param flag whether to start with combining factors or binding rows
 #' @return a tree with the defined level as a dataframe
 #'
 #' @importFrom  dplyr bind_rows
-fixPeriods <- function(x,depth=5) {
-  if(depth==0) {
-    do.call(dplyr::bind_rows,
-      if(length(x)>1) {
-        lapply(x, data.frame)
-      } else {
-        list(data.frame(x))
-      })
-  } else {
-    lapply(x, function(element) {
-      if('list' %in% class(element)) fixPeriods(element,depth-1) else element
-    })
-  }
+JSONtoDF <- function(x,depth=5,flag=TRUE) {
+    if(depth==0) {
+      dplyr::bind_rows(if(length(x)>1) lapply(x, data.frame) else list(data.frame(x)))
+    } else if(flag) {
+      cbindNames(lapply(x, function(element) if('list' %in% class(element)) JSONtoDF(element,depth-1,flag = !flag) else element))
+    } else {
+      dplyr::bind_rows(lapply(x, function(element) if('list' %in% class(element)) JSONtoDF(element,depth-1,flag = !flag) else element))
+    }
 }
 
 
-
-
-#'Combines the list at given depth with the factors associated with that depth
+#' Combines elements of an object, keeping names
 #'
-#' @param x A list
-#' @param depth Number of levels of sublists
+#' @param x 
 #'
-#' @return a tree with the factors at the defined level combined with a df
-#'
-combineFactors <- function(x,depth=4) {
-  if(depth==0) {
-    nameslist <- sapply(names(x), function(elename) {
-      if(length(x[[elename]]) != 0){if('list' %in% typeof(x[elename][[1]])) names(x[elename][[1]]) else elename} else NULL})
-    
-    result <- Reduce(function(a,b) if(length(a) !=0 & length(b)!=0 ) data.frame(a,b) else c(a,b),x)
-    names(result) <- unlist(nameslist)
-    data.frame(result)
-  } else {
-    lapply(x, function(element) {
-      if('list' %in% class(element)) combineFactors(element,depth-1) else element
-    })
-  }
+#' @return data.frame containing combined names
+#' @export
+cbindNames <- function(x) {
+  namesList <- sapply(names(x), function(elename) {
+    if(length(x[[elename]]) != 0) {
+           if('list' %in% typeof(x[elename][[1]])) names(x[elename][[1]]) 
+           else elename} else NULL})
   
-  
+  result <- Reduce(function(a,b) if(length(a) !=0 & length(b)!=0) data.frame(a,b) else c(a,b), x)
+  names(result) <- unlist(namesList)
+  data.frame(result)
 }
+
 
 
