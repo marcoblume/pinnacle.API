556b6455655b9cc7b730ccfb1391ae4d63b78a27
Aaron Jacobs <aaron.jacobs@crescendotechnology.com>
2018-01-31 21:22:56 +0000

    Adds tests and fixes a paramater issue for `GetFixtures()`.
    Signed-off-by: Aaron Jacobs <aaron.jacobs@crescendotechnology.com>

--- tests/testthat/test-basic-functionality.R
+++ tests/testthat/test-basic-functionality.R
@@ -48,3 +48,43 @@ testthat::test_that("GetBettingStatus() returns the expected format", {
                                       "ALL_LIVE_BETTING_CLOSED",
                                       "ALL_BETTING_CLOSED"))
 })
+
+# Straight Lines --------------------------------------------------------------
+testthat::context("Straight Lines")
+
+testthat::test_that("GetFixtures() returns the expected format", {
+  fixtures <- pinnacle.API::GetFixtures(config$sport)
+
+  testthat::expect_is(fixtures, "data.frame")
+  testthat::expect_gt(nrow(fixtures), 0)
+
+  # Pick some events to test filtering calls against.
+  set.seed(101)
+  events <- sample.int(nrow(fixtures), 2)
+  events <- fixtures[events, c("league.id", "league.events.id")]
+  names(events) <- c("league", "event")
+
+  result <- pinnacle.API::GetFixtures(config$sport, eventids = events$event)
+  testthat::expect_equal(nrow(result), 2)
+
+  # Mismatched league/event IDs should give no results.
+  result <- pinnacle.API::GetFixtures(config$sport, eventids = events$event[1],
+                                      leagueids = events$league[1] + 1)
+  testthat::expect_equal(nrow(result), 0)
+
+  # Adding an invalid event ID should error.
+  testthat::expect_error(
+    pinnacle.API::GetFixtures(config$sport, eventids = c(-1, events$event)),
+    regexp = "Invalid request parameters."
+  )
+
+  # As should the addition of a second sport.
+  testthat::expect_error(
+    pinnacle.API::GetFixtures(c(config$sport, 1e6), eventids = events$event),
+    regexp = "Only one sport can be specified at a time."
+  )
+
+  # Some odd event IDs are allowed, though.
+  result <- pinnacle.API::GetFixtures(config$sport, eventids = 0)
+  testthat::expect_equal(nrow(result), 0)
+})
